12422c2fbae7fe10a0796907b2e2119814026a98
gabor <g.fenyvesi@bbqberlin.onmicrosoft.com>
2023-02-06 10:54:57 +0100

    ajax ready on backend side.

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 include 'config.php';
-include 'view/customer.php';
-
-
 spl_autoload_register(function ($class)
 {
     include 'class/' . $class . '.php';
 });
 
-$monday = '2023-01-31';
-$week = Termin::getWeek($monday);
-
-echo "<pre>";
-print_r($week);
-echo "</pre>";
-
+include 'view/' . $view . '.php';
