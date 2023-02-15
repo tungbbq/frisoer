0ded32bd8c15709964e4c490dd9420d06d629d23
TungNguyen <yrtyr@ff.com>
2023-02-15 15:01:01 +0100

    function barberWorkSchedule reworked and started with function deleteAppointment

--- ajax.php
+++ ajax.php
@@ -1,17 +1,22 @@
 <?php
 include './config.php';
 
-
 spl_autoload_register(function ($class)
 {
     include 'classes/' . $class . '.php';
 });
-$monday = $_POST['monday'];
-$barber_id = $_POST['barber_id'] ?? '';
+$monday = $_POST['monday'] ?? '';
+$barberId = $_POST['barberId'] ?? '';
+$delete = $_POST['delete'] ?? '';
+$getAllBarbers = $_POST['getAllBarbers'] ?? '';
 
+$transferredWeek = Appointment::getAppointmentsByBarber($monday, (int)$barberId);
 
-//print_r($_POST);
-$transferredWeek = Appointment::getAppointmentsByBarber($monday, (int)$barber_id);
+if ($getAllBarbers !== ''){
+    $getBarbers = User::getNamesOfBarbers();
+    echo json_encode($getBarbers);
+} else
 
 echo json_encode($transferredWeek);
+
 
