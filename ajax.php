933075e3dc2b669005b93ed263a0f7db242b6a23
TungNguyen <yrtyr@ff.com>
2023-03-01 10:45:55 +0100

    style: 2 password in ajax.php, remove 1

--- ajax.php
+++ ajax.php
@@ -20,7 +20,6 @@ $firstName = $_POST['firstName'] ?? '';
 $lastName = $_POST['lastName'] ?? '';
 $password = $_POST['password'] ?? '';
 $telephone = $_POST['telephone'] ?? '';
-$password = $_POST['telephone'] ?? '';
 $workStart = $_POST['workStart'] ?? '';
 $workEnd = $_POST['workEnd'] ?? '';
 $roleToSave = $_POST['roleToSave'] ?? '';
